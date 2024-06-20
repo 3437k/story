c6326fdf132fd9146592e785197363f7aaafa549
Hyukjin Kwon <insort@naver.com>
2024-06-21 01:04:13 +0900

    add server orange template

---
+++ server/orange/src/main/java/com/k3437/orange/OrangeApplication.java
@@ -0,0 +1,13 @@
+package com.k3437.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeApplication.class, args);
+	}
+
+}
---
+++ server/orange/src/test/java/com/k3437/orange/OrangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k3437.orange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
