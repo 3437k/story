5998f2328a9ae02e9fcd50588e9f6982a3058e8b
Hyukjin Kwon <insort@naver.com>
2024-06-20 11:02:29 +0900

    add springboot template

---
+++ server/june/src/test/java/com/_k/june/JuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com._k.june;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
